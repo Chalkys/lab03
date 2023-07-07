4f2eaf34973c1dff752e3da70fd014b4618a481f
Alex (Pahom) <deholas@mail.ru>
2023-07-07 03:45:28 +0300

    Update: Добавлено отображение диаграммы

--- main.cpp
+++ main.cpp
@@ -5,6 +5,57 @@
 #include<iomanip>
 #include <algorithm>
 using namespace std;
+void svg_rect(double x, double y, double width, double height, string stroke = "blue", string fill = "blue");
+
+void svg_begin(double width, double height) {
+	cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+	cout << "<svg ";
+	cout << "width='" << width << "' ";
+	cout << "height='" << height << "' ";
+	cout << "viewBox='0 0 " << width << " " << height << "' ";
+	cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void svg_end() {
+	cout << "</svg>\n";
+}
+
+void svg_text(double left, double baseline, string text) {
+	cout << "<text x='20' y='35'>anything you want</text>";
+}
+
+void show_histogram_svg(const vector<size_t>& bins) {
+
+	const auto IMAGE_WIDTH = 400;
+	const auto IMAGE_HEIGHT = 300;
+	const auto TEXT_LEFT = 20;
+	const auto TEXT_BASELINE = 20;
+	const auto TEXT_WIDTH = 50;
+	const auto BIN_HEIGHT = 30;
+	const auto BLOCK_WIDTH = 10;
+
+	// начало документа
+	svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+
+	// гистограмма
+	double top = 0;
+	size_t cnt = 0;
+	for (size_t bin = 0; bin < bins.size() - 1; bin++) {
+		cnt += bins[bin];
+		const double bin_width = BLOCK_WIDTH * cnt;
+		svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+		svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "red", "#ffeeee");
+		top += BIN_HEIGHT;
+	}
+
+	// конец документа
+	svg_end();
+}
+
+void svg_rect(double x, double y, double width, double height, string stroke, string fill) {
+	cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << "'/>\n";
+	//cout << "<rect x='0' y='0' width='100' height='200' />";
+}
 
 vector<size_t> input1(int n) {
 	vector<size_t> bins(n);
@@ -14,7 +65,7 @@ vector<size_t> input1(int n) {
 	return bins;
 }
 
-size_t *max_min(vector<size_t> v1, int m) {
+size_t max_min(vector<size_t> v1, int m) {
 	size_t min = v1[0], max = v1[0];
 
 	for (int i = 1; i < v1.size(); i++) {
@@ -27,15 +78,15 @@ size_t *max_min(vector<size_t> v1, int m) {
 	}
 
 	if (m == 0) {
-		return &min;
+		return min;
 	}
 	else if (m == 1)
-		return &max;
+		return max;
 }
 
 vector<size_t> gist(vector<size_t> v, double bin_count) {
 
-	double diff = (*max_min(v, 1) - *max_min(v, 0)) / bin_count;
+	double diff = (max_min(v, 1) - max_min(v, 0)) / bin_count;
 
 
 	vector<size_t> fin;
@@ -49,7 +100,7 @@ vector<size_t> gist(vector<size_t> v, double bin_count) {
 
 		if (flag) {
 			for (int j = 0; j < v.size(); j++) {
-				if (v[j] >= (*max_min(v, 0) + diff * i) && v[j] <= (*max_min(v, 0) + diff * (i + 1)) && check_fin[j] == 0) {
+				if (v[j] >= (max_min(v, 0) + diff * i) && v[j] <= (max_min(v, 0) + diff * (i + 1)) && check_fin[j] == 0) {
 					cnt++;
 					check_fin[j] = 1;
 				}
@@ -74,14 +125,14 @@ vector<size_t> gist(vector<size_t> v, double bin_count) {
 	for (int i = 0; i < fin.size(); i++) {
 		if (i == 0) {
 			new_fin.push_back(fin[i]);
-			cout << new_fin[i];
+			//cout << new_fin[i];
 		}
 		else {
 			new_fin.push_back(fin[i] - fin[i - 1]);
-			cout << '-' << new_fin[i];
+			//cout << '-' << new_fin[i];
 		}
 	}
-	cout << endl;
+	//cout << endl;
 
 	new_fin.push_back(new_max);
 
@@ -102,8 +153,11 @@ int main() {
 
 	vector<size_t> new_fin = gist(bins, bin_count);
 
+
 	size_t height;
 	size_t cnt = 0;
+
+	/*
 	for (int i = 0; i < new_fin.size() - 1; i++) {
 
 
@@ -124,5 +178,7 @@ int main() {
 
 		cout << endl;
 	}
+	*/
+	show_histogram_svg(new_fin);
 	return 0;
 }
